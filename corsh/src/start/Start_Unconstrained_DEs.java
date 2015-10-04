8445aa444cf06e209a8e5ca8f8e2bcfe91218f48
david breetzke <u12056503@tuks.co.za>
2015-10-04 08:36:43 +0200

    Added all unconstrained problems to DE Uncon. starter

--- corsh/src/start/Start_Unconstrained_DEs.java
+++ corsh/src/start/Start_Unconstrained_DEs.java
@@ -19,33 +19,37 @@ public class Start_Unconstrained_DEs {
         AbstractProblem[] problems = getProblemsList();
         Simulation sim = new Simulation(algorithms, problems, 10000);
         sim.run();
-
-        //Run Simulation
-
-        //    System.out.println("Starting " + algorithms[i].getName()); ADD THIS TO SIMULATION CLASS
-        
     }
 
 
     private static AbstractProblem[] getProblemsList() {
-        AbstractProblem[] problems = new AbstractProblem[16];
+        AbstractProblem[] problems = new AbstractProblem[25];
 
         problems[0] = new U_Ackley();
-        problems[1] = new U_Beale();
-        problems[2] = new U_Bohachevsky();
-        problems[3] = new U_GoldsteinPrice();
-        problems[4] = new U_Griewank();
-        problems[5] = new U_Quadric();
-        problems[6] = new U_Quartic();
-        problems[7] = new U_Rana();
-        problems[8] = new U_Rastigrin();
-        problems[9] = new U_Rosenbrock();
-        problems[10] = new U_Salomon();
-        problems[11] = new U_Schwefel2_22();
-        problems[12] = new U_Schwefel2_26();
-        problems[13] = new U_SixHumpCamelBack();
-        problems[14] = new U_Spherical();
-        problems[15] = new U_Step();
+        problems[1] = new U_Alpine();
+        problems[2] = new U_Beale();
+        problems[3] = new U_Bohachevsky();
+        problems[4] = new U_GoldsteinPrice();
+        problems[5] = new U_EggHolder();
+        problems[6] = new U_GoldsteinPrice();
+        problems[7] = new U_Griewank();
+        problems[8] = new U_Levy();
+        problems[9] = new U_Michalewicz();
+        problems[10] = new U_Pathological();
+        problems[11] = new U_Quadric();
+        problems[12] = new U_Quartic();
+        problems[13] = new U_Rana();
+        problems[14] = new U_Rastigrin();
+        problems[15] = new U_Rosenbrock();
+        problems[16] = new U_Salomon();
+        problems[17] = new U_Schwefel2_22();
+        problems[18] = new U_Schwefel2_26();
+        problems[19] = new U_SixHumpCamelBack();
+        problems[20] = new U_SkewRastrigin();
+        problems[21] = new U_Spherical();
+        problems[22] = new U_Step();
+        problems[23] = new U_Weierstrass();
+        problems[24] = new U_Zakharov();
         return problems;
     }
 
